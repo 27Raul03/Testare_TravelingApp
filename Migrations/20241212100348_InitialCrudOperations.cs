528e763865d6e05f84fcdc441b2fc7da389b6706
IDEAPAD-FLEX-5\cadar <cadarraulvladut27@gmail.com>
2024-12-12 17:19:04 +0200

    Migrari

---
+++ Migrations/20241212100348_InitialCrudOperations.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Testare_TravelingApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCrudOperations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Activity",
+                columns: table => new
+                {
+                    ActivityId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    EndDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Activity", x => x.ActivityId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Agenda",
+                columns: table => new
+                {
+                    AgendaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DateCreated = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Agenda", x => x.AgendaId);
+                    table.ForeignKey(
+                        name: "FK_Agenda_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "NatureTrail",
+                columns: table => new
+                {
+                    NatureTrailId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LengthInKm = table.Column<double>(type: "float", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_NatureTrail", x => x.NatureTrailId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Restaurant",
+                columns: table => new
+                {
+                    RestaurantId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Cuisine = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Restaurant", x => x.RestaurantId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TouristAttraction",
+                columns: table => new
+                {
+                    TouristAttractionId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(200)", maxLength: 200, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    OpeningHours = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TouristAttraction", x => x.TouristAttractionId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "AgendaActivity",
+                columns: table => new
+                {
+                    AgendaActivityId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    AgendaId = table.Column<int>(type: "int", nullable: false),
+                    ActivityId = table.Column<int>(type: "int", nullable: true),
+                    RestaurantId = table.Column<int>(type: "int", nullable: true),
+                    NatureTrailId = table.Column<int>(type: "int", nullable: true),
+                    TouristAttractionId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AgendaActivity", x => x.AgendaActivityId);
+                    table.ForeignKey(
+                        name: "FK_AgendaActivity_Activity_ActivityId",
+                        column: x => x.ActivityId,
+                        principalTable: "Activity",
+                        principalColumn: "ActivityId");
+                    table.ForeignKey(
+                        name: "FK_AgendaActivity_Agenda_AgendaId",
+                        column: x => x.AgendaId,
+                        principalTable: "Agenda",
+                        principalColumn: "AgendaId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_AgendaActivity_NatureTrail_NatureTrailId",
+                        column: x => x.NatureTrailId,
+                        principalTable: "NatureTrail",
+                        principalColumn: "NatureTrailId");
+                    table.ForeignKey(
+                        name: "FK_AgendaActivity_Restaurant_RestaurantId",
+                        column: x => x.RestaurantId,
+                        principalTable: "Restaurant",
+                        principalColumn: "RestaurantId");
+                    table.ForeignKey(
+                        name: "FK_AgendaActivity_TouristAttraction_TouristAttractionId",
+                        column: x => x.TouristAttractionId,
+                        principalTable: "TouristAttraction",
+                        principalColumn: "TouristAttractionId");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Review",
+                columns: table => new
+                {
+                    ReviewId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Rating = table.Column<int>(type: "int", nullable: false),
+                    Comment = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    DatePosted = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    ActivityId = table.Column<int>(type: "int", nullable: true),
+                    RestaurantId = table.Column<int>(type: "int", nullable: true),
+                    NatureTrailId = table.Column<int>(type: "int", nullable: true),
+                    TouristAttractionId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Review", x => x.ReviewId);
+                    table.ForeignKey(
+                        name: "FK_Review_Activity_ActivityId",
+                        column: x => x.ActivityId,
+                        principalTable: "Activity",
+                        principalColumn: "ActivityId");
+                    table.ForeignKey(
+                        name: "FK_Review_NatureTrail_NatureTrailId",
+                        column: x => x.NatureTrailId,
+                        principalTable: "NatureTrail",
+                        principalColumn: "NatureTrailId");
+                    table.ForeignKey(
+                        name: "FK_Review_Restaurant_RestaurantId",
+                        column: x => x.RestaurantId,
+                        principalTable: "Restaurant",
+                        principalColumn: "RestaurantId");
+                    table.ForeignKey(
+                        name: "FK_Review_TouristAttraction_TouristAttractionId",
+                        column: x => x.TouristAttractionId,
+                        principalTable: "TouristAttraction",
+                        principalColumn: "TouristAttractionId");
+                    table.ForeignKey(
+                        name: "FK_Review_User_UserId",
+                        column: x => x.UserId,
+                        principalTable: "User",
+                        principalColumn: "UserId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Agenda_UserId",
+                table: "Agenda",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AgendaActivity_ActivityId",
+                table: "AgendaActivity",
+                column: "ActivityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AgendaActivity_AgendaId",
+                table: "AgendaActivity",
+                column: "AgendaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AgendaActivity_NatureTrailId",
+                table: "AgendaActivity",
+                column: "NatureTrailId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AgendaActivity_RestaurantId",
+                table: "AgendaActivity",
+                column: "RestaurantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AgendaActivity_TouristAttractionId",
+                table: "AgendaActivity",
+                column: "TouristAttractionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Review_ActivityId",
+                table: "Review",
+                column: "ActivityId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Review_NatureTrailId",
+                table: "Review",
+                column: "NatureTrailId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Review_RestaurantId",
+                table: "Review",
+                column: "RestaurantId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Review_TouristAttractionId",
+                table: "Review",
+                column: "TouristAttractionId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Review_UserId",
+                table: "Review",
+                column: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AgendaActivity");
+
+            migrationBuilder.DropTable(
+                name: "Review");
+
+            migrationBuilder.DropTable(
+                name: "Agenda");
+
+            migrationBuilder.DropTable(
+                name: "Activity");
+
+            migrationBuilder.DropTable(
+                name: "NatureTrail");
+
+            migrationBuilder.DropTable(
+                name: "Restaurant");
+
+            migrationBuilder.DropTable(
+                name: "TouristAttraction");
+        }
+    }
+}

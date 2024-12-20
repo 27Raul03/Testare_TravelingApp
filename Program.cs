f4a3297030396f934c7de48e63c8c91e5724ae1f
Melisa <mocan.melisa.maria@gmail.com>
2024-12-20 21:44:10 +0200

    login/register

--- Program.cs
+++ Program.cs
@@ -1,20 +1,34 @@
-﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Testare_TravelingApp.Data;
 using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddRazorPages();
 builder.Services.AddDbContext<Testare_TravelingAppContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Testare_TravelingAppContext") ?? throw new InvalidOperationException("Connection string 'Testare_TravelingAppContext' not found.")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Testare_TravelingAppContext")
+    ?? throw new InvalidOperationException("Connection string 'Testare_TravelingAppContext' not found.")));
+
+builder.Services.AddDbContext<LibraryIdentityContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Testare_TravelingAppContext")
+    ?? throw new InvalidOperationException("Connection string 'Testare_TravelingAppContext' not found.")));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = true;
+    options.Password.RequireDigit = true;
+    options.Password.RequiredLength = 6;
+    options.Password.RequireNonAlphanumeric = false;
+})
+.AddEntityFrameworkStores<LibraryIdentityContext>();
+
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -22,7 +36,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapRazorPages();
